2e50fc6b7e315420f7dd0b94a96bd8edb49f2d2d
seatletommorow <flufywww@gmail.com>
2026-07-31 00:33:38 +0300

    Revert "New Painters"
    This reverts commit e7ac49b4c5176de696aeb1914714ce805d84e780.
    
    (cherry picked from commit 56abe339376b19dc23f43f3f066bf8b97eb059c4)

--- System.Windows.Forms/System.Windows.Forms.Theming/Default/ButtonPainter.cs
+++ System.Windows.Forms/System.Windows.Forms.Theming/Default/ButtonPainter.cs
@@ -1,155 +1,189 @@
-// ButtonPainter.cs
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+// Copyright (c) 2007 Novell, Inc.
+//
+// Authors:
+//	Andreia Gaita (avidigal@novell.com)
+
 using System;
 using System.Drawing;
-using System.Drawing.Drawing2D;
 
 namespace System.Windows.Forms.Theming.Default
 {
-    internal class ButtonPainter
-    {
-        public ButtonPainter()
-        {
-        }
+	/// <summary>
+	/// Summary description for Button.
+	/// </summary>
+	internal class ButtonPainter
+	{
+		public ButtonPainter ()
+		{
 
-        protected SystemResPool ResPool { get { return ThemeEngine.Current.ResPool; } }
+		}
 
-        // Modern flat style colors
-        private static Color PrimaryColor = Color.FromArgb(25, 118, 210);
-        private static Color PrimaryHover = Color.FromArgb(21, 101, 192);
-        private static Color PrimaryPressed = Color.FromArgb(13, 71, 161);
-        private static Color BorderColor = Color.FromArgb(224, 224, 224);
-        private static Color BorderHover = Color.FromArgb(25, 118, 210);
-        private static Color TextColor = Color.FromArgb(33, 33, 33);
-        private static Color TextDisabled = Color.FromArgb(189, 189, 189);
+		protected SystemResPool ResPool { get { return ThemeEngine.Current.ResPool; } }
+		
+		#region Buttons
+		#region Standard Button
+		public virtual void Draw (Graphics g, Rectangle bounds, ButtonThemeState state, Color backColor, Color foreColor) {
+			bool is_themecolor = backColor.ToArgb () == ThemeEngine.Current.ColorControl.ToArgb () || backColor == Color.Empty ? true : false;
+			CPColor cpcolor = is_themecolor ? CPColor.Empty : ResPool.GetCPColor (backColor);
+			Pen pen;
+			
+			switch (state) {
+				case ButtonThemeState.Normal:
+				case ButtonThemeState.Entered:
+				case ButtonThemeState.Disabled:
+					pen = is_themecolor ? SystemPens.ControlLightLight : ResPool.GetPen (cpcolor.LightLight);
+					g.DrawLine (pen, bounds.X, bounds.Y, bounds.X, bounds.Bottom - 2);
+					g.DrawLine (pen, bounds.X + 1, bounds.Y, bounds.Right - 2, bounds.Y);
 
-        #region Standard Button
-        public virtual void Draw(Graphics g, Rectangle bounds, ButtonThemeState state, Color backColor, Color foreColor)
-        {
-            bool isHot = (state & ButtonThemeState.Entered) != 0;
-            bool isPressed = (state & ButtonThemeState.Pressed) != 0;
-            bool isDisabled = (state & ButtonThemeState.Disabled) != 0;
-            bool isDefault = (state & ButtonThemeState.Default) != 0;
+					pen = is_themecolor ? SystemPens.Control : ResPool.GetPen (backColor);
+					g.DrawLine (pen, bounds.X + 1, bounds.Y + 1, bounds.X + 1, bounds.Bottom - 3);
+					g.DrawLine (pen, bounds.X + 2, bounds.Y + 1, bounds.Right - 3, bounds.Y + 1);
 
-            g.SmoothingMode = SmoothingMode.AntiAlias;
-            g.TextRenderingHint = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+					pen = is_themecolor ? SystemPens.ControlDark : ResPool.GetPen (cpcolor.Dark);
+					g.DrawLine (pen, bounds.X + 1, bounds.Bottom - 2, bounds.Right - 2, bounds.Bottom - 2);
+					g.DrawLine (pen, bounds.Right - 2, bounds.Y + 1, bounds.Right - 2, bounds.Bottom - 3);
 
-            Color bgColor, borderColor, fgColor;
-            int radius = 4;
+					pen = is_themecolor ? SystemPens.ControlDarkDark : ResPool.GetPen (cpcolor.DarkDark);
+					g.DrawLine (pen, bounds.X, bounds.Bottom - 1, bounds.Right - 1, bounds.Bottom - 1);
+					g.DrawLine (pen, bounds.Right - 1, bounds.Y, bounds.Right - 1, bounds.Bottom - 2);
+					break;
+				case ButtonThemeState.Pressed:
+					g.DrawRectangle (ResPool.GetPen (foreColor), bounds.X, bounds.Y, bounds.Width - 1, bounds.Height - 1);
 
-            if (isDisabled)
-            {
-                bgColor = Color.FromArgb(245, 245, 245);
-                borderColor = Color.FromArgb(210, 210, 210);
-                fgColor = TextDisabled;
-            }
-            else if (isPressed)
-            {
-                bgColor = PrimaryPressed;
-                borderColor = PrimaryPressed;
-                fgColor = Color.White;
-            }
-            else if (isDefault)
-            {
-                bgColor = PrimaryColor;
-                borderColor = PrimaryColor;
-                fgColor = Color.White;
-            }
-            else
-            {
-                bgColor = isHot ? PrimaryHover : Color.White;
-                borderColor = isHot ? BorderHover : BorderColor;
-                fgColor = isHot ? Color.White : TextColor;
-            }
+					bounds.Inflate (-1, -1);
+					pen = is_themecolor ? SystemPens.ControlDark : ResPool.GetPen (cpcolor.Dark);
+					g.DrawRectangle (pen, bounds.X, bounds.Y, bounds.Width - 1, bounds.Height - 1);
+					break;
+				case ButtonThemeState.Default:
+					g.DrawRectangle (ResPool.GetPen (foreColor), bounds.X, bounds.Y, bounds.Width - 1, bounds.Height - 1);
 
-            var rect = new Rectangle(bounds.X + 1, bounds.Y + 1, bounds.Width - 2, bounds.Height - 2);
+					bounds.Inflate (-1, -1);
+					pen = is_themecolor ? SystemPens.ControlLightLight : ResPool.GetPen (cpcolor.LightLight);
+					g.DrawLine (pen, bounds.X, bounds.Y, bounds.X, bounds.Bottom - 2);
+					g.DrawLine (pen, bounds.X + 1, bounds.Y, bounds.Right - 2, bounds.Y);
 
-            using (var path = GetRoundedRectangle(rect, radius))
-            {
-                using (var brush = new SolidBrush(bgColor))
-                    g.FillPath(brush, path);
+					pen = is_themecolor ? SystemPens.Control : ResPool.GetPen (backColor);
+					g.DrawLine (pen, bounds.X + 1, bounds.Y + 1, bounds.X + 1, bounds.Bottom - 3);
+					g.DrawLine (pen, bounds.X + 2, bounds.Y + 1, bounds.Right - 3, bounds.Y + 1);
 
-                using (var pen = new Pen(borderColor, 1))
-                    g.DrawPath(pen, path);
-            }
-        }
-        #endregion
+					pen = is_themecolor ? SystemPens.ControlDark : ResPool.GetPen (cpcolor.Dark);
+					g.DrawLine (pen, bounds.X + 1, bounds.Bottom - 2, bounds.Right - 2, bounds.Bottom - 2);
+					g.DrawLine (pen, bounds.Right - 2, bounds.Y + 1, bounds.Right - 2, bounds.Bottom - 3);
 
-        #region FlatStyle Button
-        public virtual void DrawFlat(Graphics g, Rectangle bounds, ButtonThemeState state, Color backColor, Color foreColor, FlatButtonAppearance appearance)
-        {
-            bool isHot = (state & ButtonThemeState.Entered) != 0;
-            bool isPressed = (state & ButtonThemeState.Pressed) != 0;
-            bool isDisabled = (state & ButtonThemeState.Disabled) != 0;
-            bool isDefault = (state & ButtonThemeState.Default) != 0;
+					pen = is_themecolor ? SystemPens.ControlDarkDark : ResPool.GetPen (cpcolor.DarkDark);
+					g.DrawLine (pen, bounds.X, bounds.Bottom - 1, bounds.Right - 1, bounds.Bottom - 1);
+					g.DrawLine (pen, bounds.Right - 1, bounds.Y, bounds.Right - 1, bounds.Bottom - 2);
+					break;
+			}
+		}
+		#endregion
 
-            g.SmoothingMode = SmoothingMode.AntiAlias;
-            g.TextRenderingHint = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+		#region FlatStyle Button
+		public virtual void DrawFlat (Graphics g, Rectangle bounds, ButtonThemeState state, Color backColor, Color foreColor, FlatButtonAppearance appearance) {
+			bool is_themecolor = backColor.ToArgb () == ThemeEngine.Current.ColorControl.ToArgb () || backColor == Color.Empty ? true : false;
+			CPColor cpcolor = is_themecolor ? CPColor.Empty : ResPool.GetCPColor (backColor);
+			Pen pen;
+			
+			switch (state) {
+				case ButtonThemeState.Normal:
+				case ButtonThemeState.Disabled:
+					// This will just use the BackColor
+					break;
+				case ButtonThemeState.Entered:
+				case ButtonThemeState.Default | ButtonThemeState.Entered:
+					if (appearance.MouseOverBackColor != Color.Empty)
+						g.FillRectangle (ResPool.GetSolidBrush (appearance.MouseOverBackColor), bounds);
+					else
+						g.FillRectangle (ResPool.GetSolidBrush (ChangeIntensity (backColor, .9F)), bounds);
+					break;
+				case ButtonThemeState.Pressed:
+					if (appearance.MouseDownBackColor != Color.Empty)
+						g.FillRectangle (ResPool.GetSolidBrush (appearance.MouseDownBackColor), bounds);
+					else
+						g.FillRectangle (ResPool.GetSolidBrush (ChangeIntensity (backColor, .95F)), bounds);
+					break;
+				case ButtonThemeState.Default:
+					if (appearance.CheckedBackColor != Color.Empty)
+						g.FillRectangle (ResPool.GetSolidBrush (appearance.CheckedBackColor), bounds);
+					break;
+			}
+			
+			if (appearance.BorderColor == Color.Empty)
+				pen = is_themecolor ? SystemPens.ControlDarkDark : ResPool.GetSizedPen (cpcolor.DarkDark, appearance.BorderSize);
+			else
+				pen = ResPool.GetSizedPen (appearance.BorderColor, appearance.BorderSize);
+				
+			bounds.Width -= 1;
+			bounds.Height -= 1;
+				
+			if (appearance.BorderSize > 0)
+				g.DrawRectangle (pen, bounds);
+		}
+		#endregion
 
-            Color bgColor, borderColor, fgColor;
-            int radius = 2;
+		#region Popup Button
+		public virtual void DrawPopup (Graphics g, Rectangle bounds, ButtonThemeState state, Color backColor, Color foreColor) {
+			bool is_themecolor = backColor.ToArgb () == ThemeEngine.Current.ColorControl.ToArgb () || backColor == Color.Empty ? true : false;
+			CPColor cpcolor = is_themecolor ? CPColor.Empty : ResPool.GetCPColor (backColor);
+			Pen pen;
 
-            if (isDisabled)
-            {
-                bgColor = Color.FromArgb(245, 245, 245);
-                borderColor = Color.FromArgb(210, 210, 210);
-                fgColor = TextDisabled;
-            }
-            else if (isPressed)
-            {
-                bgColor = PrimaryPressed;
-                borderColor = PrimaryPressed;
-                fgColor = Color.White;
-            }
-            else if (isDefault)
-            {
-                bgColor = PrimaryColor;
-                borderColor = PrimaryColor;
-                fgColor = Color.White;
-            }
-            else
-            {
-                bgColor = isHot ? PrimaryHover : Color.White;
-                borderColor = isHot ? BorderHover : BorderColor;
-                fgColor = isHot ? Color.White : TextColor;
-            }
+			switch (state) {
+				case ButtonThemeState.Normal:
+				case ButtonThemeState.Disabled:
+				case ButtonThemeState.Pressed:
+				case ButtonThemeState.Default:
+					pen = is_themecolor ? SystemPens.ControlDarkDark : ResPool.GetPen (cpcolor.DarkDark);
 
-            using (var path = GetRoundedRectangle(bounds, radius))
-            {
-                using (var brush = new SolidBrush(bgColor))
-                    g.FillPath(brush, path);
+					bounds.Width -= 1;
+					bounds.Height -= 1;
+					g.DrawRectangle (pen, bounds);
 
-                using (var pen = new Pen(borderColor, 1))
-                    g.DrawPath(pen, path);
-            }
-        }
-        #endregion
+					if (state == ButtonThemeState.Default || state == ButtonThemeState.Pressed) {
+						bounds.Inflate (-1, -1);
+						g.DrawRectangle (pen, bounds);
+					}
+					break;
+				case ButtonThemeState.Entered:
+					pen = is_themecolor ? SystemPens.ControlLightLight : ResPool.GetPen (cpcolor.LightLight);
+					g.DrawLine (pen, bounds.X, bounds.Y, bounds.X, bounds.Bottom - 2);
+					g.DrawLine (pen, bounds.X + 1, bounds.Y, bounds.Right - 2, bounds.Y);
 
-        #region Popup Button
-        public virtual void DrawPopup(Graphics g, Rectangle bounds, ButtonThemeState state, Color backColor, Color foreColor)
-        {
-            DrawFlat(g, bounds, state, backColor, foreColor, null);
-        }
-        #endregion
+					pen = is_themecolor ? SystemPens.ControlDark : ResPool.GetPen (cpcolor.Dark);
+					g.DrawLine (pen, bounds.X, bounds.Bottom - 1, bounds.Right - 1, bounds.Bottom - 1);
+					g.DrawLine (pen, bounds.Right - 1, bounds.Y, bounds.Right - 1, bounds.Bottom - 2);
+					break;
+			}
+		}
+		#endregion
+		#endregion
+		
+		private static Color ChangeIntensity (Color baseColor, float percent)
+		{
+			int H, I, S;
 
-        #region Helper Methods
-        private GraphicsPath GetRoundedRectangle(Rectangle rect, int radius)
-        {
-            var path = new GraphicsPath();
-            int r = Math.Min(radius, Math.Min(rect.Width / 2, rect.Height / 2));
+			ControlPaint.Color2HBS (baseColor, out H, out I, out S);
+			int NewIntensity = Math.Min (255, (int)(I * percent));
 
-            if (r <= 0)
-            {
-                path.AddRectangle(rect);
-                return path;
-            }
-
-            path.AddArc(rect.X, rect.Y, r * 2, r * 2, 180, 90);
-            path.AddArc(rect.Right - r * 2, rect.Y, r * 2, r * 2, 270, 90);
-            path.AddArc(rect.Right - r * 2, rect.Bottom - r * 2, r * 2, r * 2, 0, 90);
-            path.AddArc(rect.X, rect.Bottom - r * 2, r * 2, r * 2, 90, 90);
-            path.CloseFigure();
-            return path;
-        }
-        #endregion
-    }
+			return ControlPaint.HBS2Color (H, NewIntensity, S);			
+		}
+	}
 }
